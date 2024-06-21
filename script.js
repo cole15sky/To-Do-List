20adf2069049da3ff300465dd0621433d4d7129e
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-21 14:23:24 +0545

    Final Edit

---
+++ script.js
@@ -0,0 +1,43 @@
+let ListContainer = document.getElementById(`List-Container`);
+let InputBox = document.getElementById(`Input-box`);
+
+
+function addTask() {
+    if (InputBox.value === "") {
+        alert("You must insert something!")
+    }
+    else {
+        let li = document.createElement(`li`);
+        li.innerHTML = InputBox.value;
+        ListContainer.appendChild(li);
+        let span = document.createElement(`span`);
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    InputBox.value = "";
+
+}
+
+
+
+ListContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false)
+
+
+function saveData() {
+    localStorage.setItem(`data`, ListContainer.innerHTML);
+}
+
+
+function showTask(){
+    ListContainer.innerHTML =localStorage.getItem("data");
+}
+showTask();
